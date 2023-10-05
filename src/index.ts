1860fc5e3263d69d3aa95716374160b1ab53accf
sgrutman978 <sgrutman978@gmail.com>
2023-10-05 01:36:03 +0000

    working phantom AND email login system? new speed square table2

--- src/index.ts
+++ src/index.ts
@@ -51,10 +51,10 @@ async function mintToken(walletAddr: string, amount: number) {
 let checkAccountExists = false;
 try{
 await getAccount(connection, ata).then((obj) => {
-  console.log(obj);
+//  console.log(obj);
   checkAccountExists = obj.isInitialized;
 });
-console.log(checkAccountExists);
+//console.log(checkAccountExists);
 }catch (error) {
     console.error('ATA does not exist:', error);
   }
@@ -89,7 +89,7 @@ tx.add(
 
 app.use(express.json());
 
-
+/*
 app.post('/encrypt', async (req: Request, res: Response) => {
   try {
     return res.send({ message: encrypt.toHexString(encrypt.encryptDataRSA(req.body.amount))});
@@ -107,19 +107,31 @@ app.post('/decrypt', async (req: Request, res: Response) => {
   //} catch (error) {
     //  return res.status(500).json({ error: 'Error decrypting.' });
  // }
-});
+});*/
 
 app.post('/score', async (req: Request, res: Response) => {
   try {
-    console.log(req);
-    const amount = req.body.amount;
-    const walletAddr = req.body.walletAddr;
-    console.log(req.body);
+//    console.log(req);
+    //const score = req.body.score;
+    console.log("ooooooo");
+    const username = req.body.username;
+    console.log("lplplplplp");
+    const res3 = await client.query("SELECT login_token from users WHERE username='" + username + "';");
+    //console.log(res3);
+    const loginT = res3.rows[0].login_token;
+    console.log("loin_token: " + loginT);
+    const json = JSON.parse(encrypt.decryptGeneral(loginT, req.body.msg));
+    console.log("json");
+   console.log(json); 
+   const score = json["score"];
+
     let sql = "";
-    sql = sql + "INSERT INTO users (pub_key) SELECT ('" + walletAddr + "') WHERE NOT EXISTS (SELECT pub_key FROM users WHERE pub_key='" + walletAddr + "');";
-    sql = sql + "UPDATE users SET high_score=" + amount + " WHERE pub_key='" + walletAddr + "' AND high_score<" + amount + ";";
-    sql = sql + "UPDATE users SET total_score=total_score+" + amount + ", unclaimed_tokens=unclaimed_tokens+" + amount + " WHERE pub_key='" + walletAddr + "';";
-    sql = sql + "UPDATE users SET games_played=games_played+1 WHERE pub_key='" + walletAddr + "';";
+    //sql = sql + "INSERT INTO users (pub_key) SELECT ('" + walletAddr + "') WHERE NOT EXISTS (SELECT pub_key FROM users WHERE pub_key='" + walletAddr + "');";
+    sql = sql + "UPDATE speed_square SET high_score=" + score + " WHERE username='" + username + "' AND high_score<" + score + ";";
+    sql = sql + "UPDATE speed_square SET total_score=total_score+" + score + " WHERE username='" + username + "';";
+    sql = sql + "UPDATE speed_square SET today_score=today_score+" + score + " WHERE username='" + username + "';";
+    sql = sql + "UPDATE speed_square SET total_games=total_games+1 WHERE username='" + username + "';";
+    sql = sql + "UPDATE speed_square SET today_games=today_games+1 WHERE username='" + username + "';";
     const res2 = await client.query(sql);
     return res.send({ message: 'Scores updated successfully.'});
   } catch (error) {
@@ -127,7 +139,7 @@ app.post('/score', async (req: Request, res: Response) => {
   }
 });
 
-app.post('/claim', async (req: Request, res: Response) => {
+/*app.post('/claim', async (req: Request, res: Response) => {
   try {
       const walletAddr = req.body.walletAddr;
       const res2 = await client.query("SELECT unclaimed_tokens, claimed_today FROM users WHERE pub_key='" + walletAddr + "';");
@@ -143,7 +155,7 @@ app.post('/claim', async (req: Request, res: Response) => {
   } catch (error) {
       return res.status(500).json({ error: 'Error claiming tokens.' });
   }
-});
+});*/
 
 app.get('/player_stats', async (req: Request, res: Response) => {
   try {
@@ -152,14 +164,14 @@ app.get('/player_stats', async (req: Request, res: Response) => {
     const games_played = res2.rows[0].games_played;
     const high_score = res2.rows[0].high_score;
     const total_score = res2.rows[0].total_score;
-    console.log({ games_played: games_played, high_score: high_score, total_score: total_score});
+    //console.log({ games_played: games_played, high_score: high_score, total_score: total_score});
     return res.send({ games_played: games_played, high_score: high_score, total_score: total_score});
   } catch (error) {
       return res.status(500).json({ error: 'Error getting player stats.' });
   }
 });
 
-app.get('/claim_status', async (req: Request, res: Response) => {
+/*app.get('/claim_status', async (req: Request, res: Response) => {
   try {
     const walletAddr = req.query.walletAddr;
     const res2 = await client.query("SELECT claimed_today, unclaimed_tokens FROM users WHERE pub_key='" + walletAddr + "';");
@@ -169,13 +181,13 @@ app.get('/claim_status', async (req: Request, res: Response) => {
   } catch (error) {
       return res.status(500).json({ error: 'Error getting claim status.' });
   }
-});
+});*/
 
 
 
 app.get('/leaderboard', async (req: Request, res: Response) => {
   try {
-    const res2 = await client.query("SELECT username, high_score FROM users;");
+    const res2 = await client.query("SELECT username, high_score FROM speed_square;");
     return res.send(res2.rows);
   } catch (error) {
       return res.status(500).json({ error: 'Error getting claim status.' });
@@ -183,33 +195,115 @@ app.get('/leaderboard', async (req: Request, res: Response) => {
 });
 
 
-app.post('/login', async (req: Request, res: Response) => {
+async function sign_up(username: string, password: string, email: string, signature: string, pub_key: string, makeIdUsername: string): number {
+    try {
+    console.log("----- 1 ----");
+    let sql = "";
+    if (!pub_key || !signature) {
+	    console.log("p4");
+        sql = "INSERT INTO users (username, password, email, signature, pub_key) SELECT '" + username + "', '" + password + "', '" + email + "', '', '' WHERE NOT EXISTS (SELECT username FROM users WHERE username='" + username + "');";
+        sql = sql + "INSERT INTO speed_square (high_score, today_score, total_score, today_games, total_games, username) SELECT 0, 0, 0, 0, 0, '" + username + "' WHERE NOT EXISTS (SELECT username FROM speed_square WHERE username='" + username + "');";
+    } else {
+	    console.log("p3");
+	    //TODO verify signature and public key match cryptographically
+	sql = "INSERT INTO users (username, password, email, signature, pub_key) SELECT '" + makeIdUsername + "', '', '', '" + signature + "', '" + pub_key + "' WHERE NOT EXISTS (SELECT username FROM users WHERE username='" + makeIdUsername + "' OR pub_key='" + pub_key + "');";
+	sql = sql + "INSERT INTO speed_square (high_score, today_score, total_score, today_games, total_games, username) SELECT 0, 0, 0, 0, 0, '" + makeIdUsername + "' WHERE NOT EXISTS (SELECT username FROM users WHERE username='" + makeIdUsername + "' OR pub_key='" + pub_key + "');";
+    }
+    const res3 = await client.query(sql);
+    console.log("------2-----");
+    console.log(res3);
+    return res3.rowCount; //rows.length;
+    }catch (error) {
+      console.log(error);
+      return 0;
+  }
+}
+
+
+app.post('/signup', async (req: Request, res: Response) => {
   try {
-    console.log("mehhhhhhhh");
+	  console.log("------- signup -----");
     const msg = req.body.msg;
-    console.log("msglogin: "+msg);
     const epoch = req.body.epoch;
-    console.log("epoch: "+epoch);
     let x = encrypt.decryptLoginDataAES(epoch, msg);
-    console.log(x);
     let json = JSON.parse(x);
     const username = json.username;
     const password = json.password;
-    const login_token = makeid(48);
-    const res3 = await client.query("SELECT username, password FROM users WHERE username='" + username + "' AND password='" + password + "';");
-    if(res3.rows && res3.rows.length > 0){
-      console.log(".............4..............");
-      const res2 = await client.query("UPDATE users SET login_token='" + login_token + "' where username='" + username + "';");
-      let encRes = encrypt.encryptLoginDataAES(username, epoch, login_token);
-      console.log(encRes);
-      return res.send({encRes: encRes});
+    const email = json.email;
+    const signature = json.signature;
+    const pub_key = json.pub_key;
+    const inserted = await sign_up(username, password, email, signature, pub_key, "user"+makeid(18));
+    if(inserted > 0){
+      return res.send({success: true});
     }else{
-      return res.status(501).json({ error: 'Wrong login info moron' });
+      return res.status(500).json({ error: 'Wrong sign up info, moron' });
     }
   } catch (error) {
+      return res.status(500).json({ error: 'Error signing up.' });
+  }
+});
+
+
+app.post('/login', async (req: Request, res: Response) => {
+  try {
+    //console.log("mehhhhhhhh");
+    const msg = req.body.msg;
+   // console.log("msglogin: "+msg);
+    const epoch = req.body.epoch;
+   // console.log("epoch: "+epoch);
+    let x = encrypt.decryptLoginDataAES(epoch, msg);
+   // console.log(x);
+    let json = JSON.parse(x);
+    console.log("pizza6");
+    console.log(json);
+    const username = json.username;
+    const password = json.password;
+    console.log("pizza7");
+    const signature = json.signature;
+    const pub_key = json.pub_key;
+    const login_token = makeid(48);
+    const makeIdUsername = "user"+makeid(18);
+    let sql1 = "";
+    console.log("pizza8");
+    if(!signature || !pub_key){
+	sql1 = "SELECT username FROM users WHERE (username='" + username + "' OR email='" + username + "') AND password='" + password + "';";
+    }else{
+	    console.log("pizza1");
+	await sign_up(username, password, "", signature, pub_key, makeIdUsername);  
+	sql1 = "SELECT username FROM users WHERE signature='" + signature + "' AND pub_key='" + pub_key + "';";
+	console.log("pizza2");
+    }
+    const res3 = await client.query(sql1);
+console.log("pizza3");
+    if(res3.rows.length > 0){
+	    console.log("pizza4");
+      console.log(".............4..............");
+      let sql2 = "";
+      let usernameOrPubKey = "";
+      if(!signature || !pub_key){
+	sql2 = "UPDATE users SET login_token='" + login_token + "' WHERE username='" + username + "';";
+	usernameOrPubKey = username;
+      } else {
+	sql2 = "UPDATE users SET login_token='" + login_token + "' WHERE pub_key='" + pub_key + "';";
+        usernameOrPubKey = pub_key;
+      }
+      console.log("pizza5");
+      const res2 = await client.query(sql2);
+      let encRes = encrypt.encryptLoginDataAES(usernameOrPubKey, epoch, login_token);
+     // console.log(encRes);
+      return res.send({encRes: encRes});
+    }else{
+      console.log("1");
+      return res.send({error: "Incorrect Credentials"});
+    }
+  } catch (error) {
+	  console.log("2");
       return res.status(500).json({ error: 'Error logging in.' });
   }
 });
+
+
+app.use(express.static(__dirname + '/public'));
 
 
 function makeid(length: number) {
